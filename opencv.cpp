8be01fea648d471097b0ab36b600608a0553b98c
abilitygamer06 <63387288+abilitygamer06@users.noreply.github.com>
2021-03-28 12:57:35 +0900

    Update opencv.cpp

--- opencv.cpp
+++ opencv.cpp
@@ -14,7 +14,7 @@ void contrast2() {
 
 	float alpha = 1.f;
 	Mat dst = src + (src - 128) * alpha;
-	
+
 	imshow("src", src);
 	imshow("dst", dst);
 
@@ -24,7 +24,7 @@ void contrast2() {
 
 Mat calcGrayHist(const Mat& img) {
 	CV_Assert(img.type() == CV_8UC1);
-	
+
 	Mat hist;
 	int channels[] = { 0 };
 	int dims = 1;
@@ -66,7 +66,7 @@ void histogram_stretching() {
 
 	imshow("src", src);
 	imshow("srcHist", getGrayHistImage(calcGrayHist(src)));
-	
+
 	imshow("dst", dst);
 	imshow("dstHist", getGrayHistImage(calcGrayHist(dst)));
 
@@ -87,7 +87,7 @@ void histogram_equalization() {
 
 	imshow("src", src);
 	imshow("srcHist", getGrayHistImage(calcGrayHist(src)));
-	
+
 	imshow("dst", dst);
 	imshow("dstHist", getGrayHistImage(calcGrayHist(dst)));
 
@@ -118,7 +118,160 @@ void image_arithmetic() {
 	imshow("dst2", dst2);
 	imshow("dst3", dst3);
 	imshow("dst4", dst4);
+
+	waitKey();
+	destroyAllWindows();
+}
+
+void blurring_mean() {
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	imshow("src", src);
+
+	Mat dst;
+	for (int ksize = 3; ksize <= 7; ksize += 2) {
+		blur(src, dst, Size(ksize, ksize));
+		String desc = format("Mean: %dx%d", ksize, ksize);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+
+		imshow("dst", dst);
+		waitKey();
+	}
+
+	destroyAllWindows();
+}
+
+void blurring_gaussian() {
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	imshow("src", src);
+
+	Mat dst;
+	for (int sigma = 1; sigma <= 5; sigma++) {
+		GaussianBlur(src, dst, Size(), (double)sigma);
+		String text = format("sigma = %d", sigma);
+		putText(dst, text, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+
+		imshow("dst", dst);
+		waitKey();
+	}
+
+	destroyAllWindows();
+}
+
+void unsharp_mask() {
+	Mat src = imread("rose.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	imshow("src", src);
 	
+	for (int sigma = 1; sigma <= 5; sigma++) {
+		Mat blurred;
+		GaussianBlur(src, blurred, Size(), sigma);
+
+		float alpha = 1.f;
+		Mat dst = (1 + alpha) * src - alpha * blurred;
+
+		String desc = format("sigma, %d", sigma);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+
+		imshow("dst", dst);
+		waitKey();
+	}
+
+	destroyAllWindows();
+}
+
+void noise_gaussian() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	imshow("src", src);
+
+	for (int stddev = 10; stddev <= 30; stddev += 10) {
+		Mat noise(src.size(), CV_32SC1);
+		randn(noise, 0, stddev);
+
+		Mat dst;
+		add(src, noise, dst, Mat(), CV_8U);
+
+		String desc = format("stddev = %d", stddev);
+		putText(dst, desc, Point(10, 30), FONT_HERSHEY_SIMPLEX, 1.0, Scalar(255), 1, LINE_AA);
+		imshow("dst", dst);
+		waitKey();
+	}
+	destroyAllWindows();
+}
+
+void filter_bilateral() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	Mat noise(src.size(), CV_32SC1);
+	randn(noise, 0, 5);
+	add(src, noise, src, Mat(), CV_8U);
+	
+	Mat dst1;
+	GaussianBlur(src, dst1, Size(), 5);
+
+	Mat dst2;
+	bilateralFilter(src, dst2, -1, 10, 5);
+
+	imshow("src", src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+
+	waitKey();
+	destroyAllWindows();
+}
+
+void filter_median() {
+	Mat src = imread("lenna.bmp", IMREAD_GRAYSCALE);
+
+	if (src.empty()) {
+		cerr << "Image load failed!" << endl;
+		return;
+	}
+
+	int num = (int)(src.total() * 0.1);
+	for (int i = 0; i < num; i++) {
+		int x = rand() % src.cols;
+		int y = rand() % src.rows;
+		src.at<uchar>(y, x) = (i % 2) * 255;
+	}
+
+	Mat dst1;
+	GaussianBlur(src, dst1, Size(), 1);
+
+	Mat dst2;
+	medianBlur(src, dst2, 3);
+
+	imshow("src", src);
+	imshow("dst1", dst1);
+	imshow("dst2", dst2);
+
 	waitKey();
 	destroyAllWindows();
 }
@@ -128,11 +281,17 @@ void choice_cv(int a) {
 	if (a == 1) histogram_stretching();
 	if (a == 2) histogram_equalization();
 	if (a == 3) image_arithmetic();
+	if (a == 4) blurring_mean();
+	if (a == 5) blurring_gaussian();
+	if (a == 6) unsharp_mask();
+	if (a == 7) noise_gaussian();
+	if (a == 8) filter_bilateral();
+	if (a == 9) filter_median();
 }
 
 int main(void) {
 	int a;
-	cout << "���Ϻ�2 : 0 \n������׷� ��Ʈ��Ī : 1 \n������׷� ��Ȱȭ : 2 \n���� ��� : 3" << endl;
+	cout << "명암비2 : 0 \n히스토그램 스트레칭 : 1 \n히스토그램 평활화 : 2 \n영상 산술 : 3 \n평균값블러링 : 4 \n가우시안필터링 : 5 \n언샤프마스크 : 6 \n잡음추가 : 7 \n양방향필터링 : 8 \n미디언필터링 : 9"<< endl;
 	cin >> a;
 	choice_cv(a);
 	return 0;
