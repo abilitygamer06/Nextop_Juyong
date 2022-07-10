356242b79053ae89c44e3c44101fed911aadafde
abilitygamer06 <63387288+abilitygamer06@users.noreply.github.com>
2022-07-10 19:39:04 +0900

    Add files via upload

---
+++ search_doc.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void) {
+	// 첫째 줄에 오는 문서
+	string a;
+	// 둘째 줄에 오는 문서
+	string b;
+	// 동일한 문자 확인
+	int checking = 0;
+	// 결과값
+	int answer = 0;
+
+	getline(cin, a);
+	getline(cin, b);
+
+	for (int i = 0; i < a.size(); i++) {
+		for (int j = 0; j < b.size(); j++) {
+			if (a[i] == b[j]) {
+				checking++;
+				if (checking == b.size()) {
+					answer++;
+					checking = 0;
+				}
+				if (i == a.size()-1) {
+					cout << answer << endl;
+					return 0;
+				}
+				i++;
+				continue;
+			}
+			if (a[i] != b[j]) {
+				if (checking != 0) checking = 0;
+				if (checking == b.size()) {
+					answer++;
+					checking = 0;
+				}
+				if (i == a.size()-1) {
+					cout << answer << endl;
+					return 0;
+				}
+				i++;
+				continue;
+			}
+		}
+		i--;
+	}
+}
